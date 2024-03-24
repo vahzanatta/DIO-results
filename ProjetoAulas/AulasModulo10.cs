d8ef80b4dd3e50e97cbab286258f21637df7b669
Felipe de Almeida <felipe.allmeida.dev@gmail.com>
2024-03-24 20:37:09 -0300

    changed code to dont use minimal api namespace

--- ProjetoAulas/AulasModulo10.cs
+++ ProjetoAulas/AulasModulo10.cs
@@ -1,89 +1,91 @@
 using System.Runtime.CompilerServices;
 
-namespace Modulo10;
-
-public class TrabalhandoComStrings
+namespace Modulo10
 {
-    public void ConverterParaLetrasMinusculas()
+
+    public class TrabalhandoComStrings
     {
-        Console.Write("Favor digitar alguma informacao: ");
-        var linha = Console.ReadLine();
-        Console.WriteLine(linha.ToLower());
-    }
-
-    public void ConverterParaLetrasMaiusculas()
-    {
-        Console.Write("Favor digitar alguma informacao: ");
-        var linha = Console.ReadLine();
-        Console.WriteLine(linha.ToUpper());
-    }
-
-    public void AulaSubstring()
-    {
-        Console.Write("Favor digitar alguma informacao: ");
-        var linha = Console.ReadLine();
-        Console.WriteLine(linha.Substring(0, 7));
-    }
-
-    public void AulaRange()
-    {
-        string nomeArquivo = "2024_02_28_backup.bak";
-
-        var ano = nomeArquivo[..4];
-        Console.WriteLine(ano);
-
-        string extensao = nomeArquivo[^3..];
-        Console.WriteLine(extensao);
-
-        string nome = nomeArquivo[11..^4];
-        Console.WriteLine(nome);
-
-        string apenasName = nomeArquivo[..^4];
-        Console.WriteLine(apenasName);
-    }
-
-    public void AulaContains()
-    {
-        string nomeArquivo = "2024_02_28_backup.bak";
-        //Console.WriteLine("Contem nome: " + nomeArquivo.Contains("backup"));
-
-        if(nomeArquivo.Contains("backup"))
+        public void ConverterParaLetrasMinusculas()
         {
-            Console.WriteLine("Palavra encontrada!");
+            Console.Write("Favor digitar alguma informacao: ");
+            var linha = Console.ReadLine();
+            Console.WriteLine(linha.ToLower());
         }
-        else
+
+        public void ConverterParaLetrasMaiusculas()
         {
-            Console.WriteLine("Palavra nao encontrada!");
+            Console.Write("Favor digitar alguma informacao: ");
+            var linha = Console.ReadLine();
+            Console.WriteLine(linha.ToUpper());
         }
-    }
 
-    public void AulaTrim()
-    {
-        string teste = "**VANESSA ZANATTA**";
+        public void AulaSubstring()
+        {
+            Console.Write("Favor digitar alguma informacao: ");
+            var linha = Console.ReadLine();
+            Console.WriteLine(linha.Substring(0, 7));
+        }
 
-        Console.WriteLine("TOTAL: " + teste.Trim('*'));
-        Console.WriteLine("INICIO: " + teste.TrimStart('*'));
-        Console.WriteLine("FINAL: " + teste.TrimEnd('*'));
-    }
+        public void AulaRange()
+        {
+            string nomeArquivo = "2024_02_28_backup.bak";
 
-    public void AulaStartWithEndWith()
-    {
-        string teste = "Curso Csharp";
-        Console.WriteLine("INICIO: " + teste.StartsWith("Curso"));
-        Console.WriteLine("FINAL: " + teste.EndsWith("Csharp02"));
-    }
+            var ano = nomeArquivo[..4];
+            Console.WriteLine(ano);
 
-    public void AulaReplace()
-    {
-        string teste = "Curso Csharp";
-        Console.WriteLine(teste);
-        Console.WriteLine(teste.Replace("Csharp", "C#"));
-    }
+            string extensao = nomeArquivo[^3..];
+            Console.WriteLine(extensao);
 
-    public void AulaLength ()
-    {
-        string teste = Console.ReadLine();
-        Console.WriteLine(teste);
-        Console.WriteLine(teste.Length);
+            string nome = nomeArquivo[11..^4];
+            Console.WriteLine(nome);
+
+            string apenasName = nomeArquivo[..^4];
+            Console.WriteLine(apenasName);
+        }
+
+        public void AulaContains()
+        {
+            string nomeArquivo = "2024_02_28_backup.bak";
+            //Console.WriteLine("Contem nome: " + nomeArquivo.Contains("backup"));
+
+            if (nomeArquivo.Contains("backup"))
+            {
+                Console.WriteLine("Palavra encontrada!");
+            }
+            else
+            {
+                Console.WriteLine("Palavra nao encontrada!");
+            }
+        }
+
+        public void AulaTrim()
+        {
+            string teste = "**VANESSA ZANATTA**";
+
+            Console.WriteLine("TOTAL: " + teste.Trim('*'));
+            Console.WriteLine("INICIO: " + teste.TrimStart('*'));
+            Console.WriteLine("FINAL: " + teste.TrimEnd('*'));
+        }
+
+        public void AulaStartWithEndWith()
+        {
+            string teste = "Curso Csharp";
+            Console.WriteLine("INICIO: " + teste.StartsWith("Curso"));
+            Console.WriteLine("FINAL: " + teste.EndsWith("Csharp02"));
+        }
+
+        public void AulaReplace()
+        {
+            string teste = "Curso Csharp";
+            Console.WriteLine(teste);
+            Console.WriteLine(teste.Replace("Csharp", "C#"));
+        }
+
+        public void AulaLength()
+        {
+            string teste = Console.ReadLine();
+            Console.WriteLine(teste);
+            Console.WriteLine(teste.Length);
+        }
     }
 }
